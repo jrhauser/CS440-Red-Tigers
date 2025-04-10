6d753621e04f558de8f878096bf18fed2f725b60
jrhauser <jrh00045@mix.wvu.edu>
2025-04-10 15:01:30 -0400

    Created django app, gitignore, mysql connection

--- PC_Part_Store/settings.py
+++ PC_Part_Store/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'PC_Part_Store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'RedTiger',
+        'USER':'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
