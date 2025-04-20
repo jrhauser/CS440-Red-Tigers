abc51d152f248b6c73a864abc1a4ad08fe87a486
gribelUS <tiagobreunig1@gmail.com>
2025-04-20 18:00:16 -0400

    model

--- RedTiger/models.py
+++ RedTiger/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Hello(models.Model):
@@ -38,4 +39,4 @@ class Listing(models.Model):
     deviceID = models.ForeignKey(Device, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveSmallIntegerField()
-    sellerID = models.ForeignKey('User', on_delete=models.CASCADE)
+    sellerID = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
