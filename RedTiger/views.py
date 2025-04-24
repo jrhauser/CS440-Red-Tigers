bcda40bbe4c77338cc640228b5c131dca9d3483b
jrhauser <jrh00045@mix.wvu.edu>
2025-04-24 15:23:14 -0400

    added checkout page

--- RedTiger/views.py
+++ RedTiger/views.py
@@ -5,3 +5,7 @@ from django.template import loader
 def index(request):
     template = loader.get_template("redtiger/index.html")
     return HttpResponse(template.render())
+
+def checkout(request):
+    template = loader.get_template("redtiger/checkout.html")
+    return HttpResponse(template.render())
