5909eed098afd43c18ac1bd96da5c1979c741d2f
jrhauser <jrh00045@mix.wvu.edu>
2025-04-24 14:52:26 -0400

    change urls, add django bootstrap

--- RedTiger/views.py
+++ RedTiger/views.py
@@ -3,8 +3,5 @@ from .models import Hello
 from django.template import loader
 
 def index(request):
-    text = Hello.objects.raw("SELECT * FROM RedTiger_hello")
-    return HttpResponse(text)
-def home(request):
     template = loader.get_template("redtiger/index.html")
     return HttpResponse(template.render())
