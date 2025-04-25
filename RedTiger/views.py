30e2f20d80649abe4135f1bb3fd2408dd5dac47c
jrhauser <jrh00045@mix.wvu.edu>
2025-04-25 19:01:19 -0400

    remove unused imports

--- RedTiger/views.py
+++ RedTiger/views.py
@@ -3,19 +3,7 @@ from django.contrib import auth
 from django.template import loader
 from django.db import connection
 from collections import namedtuple
-from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect
-import logging
-import unicodedata
-from django.contrib.auth.models import User
-from django import forms
-from django.contrib.auth import authenticate, get_user_model, password_validation
-from django.contrib.auth.hashers import UNUSABLE_PASSWORD_PREFIX, identify_hasher
-from django.contrib.auth.models import User
-from django.contrib.auth.tokens import default_token_generator
-from django.contrib.sites.shortcuts import get_current_site
-from django.core.exceptions import ValidationError
-from django.core.mail import EmailMultiAlternatives
 from django.template import loader
 def namedtuplefetchall(cursor):
     """
