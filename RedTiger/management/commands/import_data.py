55e067b2595800fd117c5105fa819d96fab3fa08
jrhauser <jrh00045@mix.wvu.edu>
2025-05-03 19:45:46 -0400

    added default users

--- RedTiger/management/commands/import_data.py
+++ RedTiger/management/commands/import_data.py
@@ -1,14 +1,38 @@
 import csv
+import random
 from django.core.management.base import BaseCommand
-from RedTiger.models import Device, Listing
+from RedTiger.models import Device, Listing, UserShipping
 from django.contrib.auth.models import User
 
 class Command(BaseCommand):
     help = 'Imports CPU and Motherboard data from CSV files into the Device model and creates a Listing for each device.'
 
     def handle(self, *args, **kwargs):
+        self.add_users()
         self.import_and_list('RedTiger/data/cpu_data.csv', 'CPU')
         self.import_and_list('RedTiger/data/motherboard_data.csv', 'MOBO')
+
+    def add_users(self):
+
+        user_data = [
+            ("alexjone1", "apple", "alexjone1@yahoo.com", "Alex", "Jones", "123 Maple St", "Denver", "CO"),
+            ("johnmayors8", "orange", "johnmayors8@gmail.com", "John", "Mayors", "456 Oak Ave", "Austin", "TX"),
+            ("greeen_23", "pear", "greeen_23@hotmail.com", "Christine", "Green", "789 Pine Rd", "Seattle", "WA"),
+            ("woods-tyler9", "mango", "woodstyler9@gmail.com", "Tyler", "Woods", "321 Birch Ln", "Chicago", "IL"),
+            ("franklin_gta3", "berry", "franklin_gta3@yahoo.com", "Franklin", "Lamar", "654 Cedar Dr", "Miami", "FL"),
+            ("tripper8745", "melon", "tripper8745@hotmail.com", "Trever", "Trip", "987 Spruce Ct", "Boston", "MA")
+        ]
+        for user_tuple in user_data:
+            user = User.objects.create_user(username=user_tuple[0], password=user_tuple[1], email=user_tuple[2])
+            user.first_name = user_tuple[3]
+            user.last_name = user_tuple[4]
+            user.save()
+            UserShipping.objects.get_or_create(
+                user_id = user.id,
+                shipping_address=user_tuple[5], 
+                city=user_tuple[6], 
+                state=user_tuple[7]
+            )
 
     def import_and_list(self, filepath, device_type):
         self.stdout.write(f"Importing {device_type}s from {filepath} and creating listings...")
@@ -37,7 +61,8 @@ class Command(BaseCommand):
                         if created:
                             self.stdout.write(self.style.SUCCESS(f"Imported device: {brand} {model}"))
                         # Create a listing for this device
-                        seller = User.objects.order_by('id').first()
+                        sellers = User.objects.all()
+                        seller = random.choice(sellers)
                         if seller:
                             Listing.objects.get_or_create(
                                 deviceID=device,
